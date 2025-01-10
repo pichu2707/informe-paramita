199843ee2adc43f3a044b16f0e6818040c557402
Pichu <pichu_2707@hotmail.com>
2025-01-10 09:34:51 +0100

    Subida de archivos para el seguimiento de acciones en Woocommerce

---
+++ Readme.md
@@ -0,0 +1,42 @@
+# Extracción de datos de compras de woocommerce
+
+Este script lo único que hace es unir los datos de tu ecommerce en WP para tener en una hoja las compras realizadas en WooCommerce para poder hacer luego un dashboard por ejemplo en Looker Studio y tener un seguimiento.
+Sería bueno marcar un Cron en el PC para que se actualizara de manear automática cada día, cada hora... depende ahí de los movimientos que tenga la web y la importancia de tener los datos actualizados en cada momento.
+
+
+## Librerías a utilizar
+
+Tenemos varias librearías internas de Python y otras tantas que debemos  importarlas como son:
+* collections con Counter y defaultdict
+* os
+
+Las externas a python pero dentro del abanico
+* pandas
+* requests
+* dotenv
+
+Las que son externas y están preparadas para el universo de Google
+* google.oauth2.service_account
+* gspread
+
+Hemos utilizado *dotenv* para que de esta manera no haga falta hacer público los datos que veamos más intenerantes a tener ocultos, como pueden ser la URL de la API de woocommerce por ejemplo.
+
+
+## Datos introducidos en dotenv
+
+Los datos que hemos introducido en este caso:
+* URL: es donde está la URL de la API de woocommerce
+* SPREADSHEETS_ID: Id de la hoja de Spreadsheet
+* WORKSHEET_NAME: Hoja de Spreadsheet que estamos utilizando, por ejemplo 'Hoja 1'
+* CREDS_FILE: Nombre del archivo para los tokens de petición a Google de Google Sheets 
+
+## Instalación de librerías
+
+Para instalar la librerías únicamente necesitas hacer ```pip install -r requirements.txt``` y se instalarán todas las librerías necesarias.
+
+
+### Consejo
+
+Es aconsejable crear un entorno virtual para hacer estas tareas
+
+````pip install -m venv (nombre del entorno virtual)```
---
+++ main.py
@@ -0,0 +1,92 @@
+from collections import Counter, defaultdict
+import os
+
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+
+from google.oauth2.service_account import Credentials
+import gspread
+
+load_dotenv()
+
+def get_data_from_url(url: str):
+    """Realiza la solicitud GET y defvuelve los datos JSON si es tiene éxito
+
+    Args:
+        url (str): URL de donde vamos a extraer la información
+    """
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Error al obtener los datos: {response.status_code}  - {response.text}")
+    
+def process_data(data):
+    """Procesa los datos JSON y devuelve un DataFrame listo para escribir en GSheets."""
+    contador_productos = Counter()
+    nombres_productos = {}
+    suma_precios = defaultdict(float)
+
+    for entry in data.get('datos', []):
+        if entry:  # Ignorar entradas vacías
+            producto_id = entry['producto']
+            nombre_producto = entry['nombre']
+            precio_producto = float(entry['precio'])
+
+            contador_productos[producto_id] += 1
+            nombres_productos[producto_id] = nombre_producto
+            suma_precios[producto_id] += precio_producto
+
+    #Crear lista de diccionarios para el DataFrame
+    resultados = [
+        {
+            'Producto ID': producto_id,
+            'Nombre': nombres_productos[producto_id],
+            'Cantidad de Pedidos': cantidad,
+            'Total Precio': suma_precios[producto_id]
+        }
+        for producto_id, cantidad in contador_productos.items()
+    ]
+
+    return pd.DataFrame(resultados)
+
+def update_google_sheets(df, sheet_id, worksheet_name, creds_file):
+    """Actualiza una hoja de Google Sheets con un DataFrame"""
+    SCOPES=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+                ]
+
+    creds = Credentials.from_service_account_file(
+        creds_file,
+        scopes=SCOPES,
+        )
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open_by_key(sheet_id)
+    worksheet = sheet.worksheet(worksheet_name)
+
+    #Borrar el contenido de la hoja
+    worksheet.clear()
+
+    #Escribir los datos en la hoja de cálculo
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+
+    print("Datos en Gsheet actualizados")
+
+if __name__ == "__main__":
+    #Configuración:
+    URL=os.getenv("URL")
+    SPREADSHEETS_ID=os.getenv("SPREADSHEETS_ID")
+    WORKSHEET_NAME=os.getenv("WORKSHEET_NAME")
+    CREDS_FILE=os.getenv("CREDS_FILE")
+
+    #Flujo de trabajo principal
+    try:
+        data=get_data_from_url(URL)
+        df=process_data(data)
+        update_google_sheets(df,SPREADSHEETS_ID,WORKSHEET_NAME,CREDS_FILE)
+    except Exception as e:
+        print(f"Algo no marcha bien, error: {e}")
